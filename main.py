a0bd391371ba5f059a82f4d6e03d16e94ef5e2ec
Damien Bart <49287018+bartd990@users.noreply.github.com>
2022-10-12 23:19:51 +0000

    10/12/2022

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-x = 1
-print(x)
-x_str = str(x)
+#ask for total price
+total_cost = int(
+  input("What's the total cost of the house? (Please no commas): "))
 
-print("My favorite number is", x, ".", "x=", x)
-print("My favorite number is", x_str + "." + "x=" + x_str)
-print("My favorite number is" + x_str + "." + "x=" + x_str)
+#calculate down payment and set savings to 0 and set r
+portion_down_payment = total_cost / 4
+current_savings = 0
+r = 0.04
+
+#ask salary and calculate monthly
+annual_salary = int(
+  input(
+    "What's your current annual salary? Please do not include bonuses, and please write the full amount (no commas).: "
+  ))
+monthly_salary = annual_salary / 12
+
+#ask save %
+portion_save = float(
+  input(
+    "What percentage do you wish to save? Please put it as decimal form. I.E. 100% equals 1"
+  ))
+
+#find investments
+annual_return = current_savings * r / 12
+
+annual_return = 
